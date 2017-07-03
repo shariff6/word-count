7572bfd0b94a3c81cbe7d5c917da8eaf8e9a1878
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-03 09:27:42 +0300

    this cmmit will correct the indenttation and spacing

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,11 +1,12 @@
 class  String
   define_method(:word_count) do |word|
     sentence_split = self.split()
+    extra = word
     counter = 0
     sentence_split.each do |letter|
-      word = letter.gsub(/[!,@,?,"",:,;,.]/, "")
-      if word == letter
-        counter = counter + 1
+    word = letter.gsub(/[!,@,?,"",:,;,.]/, "")
+    if word.include? extra
+        counter = counter+1
       end
     end
     counter
