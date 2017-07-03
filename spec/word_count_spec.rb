ff5f2836b1618e0810c17c8fc32158b181e2789c
shariff6 <shariff.mohammed.a@gmail.com>
2017-07-03 09:39:14 +0300

    this commit adds specs for testing if a ord that is part of a another word is counted

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,7 @@ require('rspec')
     it("counts the number of times a word has been repeated in a sentence") do
       expect(("crazy people are crazy").word_count("crazy")).to eq(2)
     end
+    it ("checks if it counts word that are part of a larger world") do
+      expect(("monkeys are monkeying around").word_count("monkey")).to eq(2)
+    end
   end
